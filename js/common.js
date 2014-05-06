9bf088685d2c61df0e9dc6177672f090334e26b6
Denis Pismenny <denis.pismenny@gmail.com>
2014-05-06 14:35:51 +0300

    tabs

--- js/common.js
+++ js/common.js
@@ -1,3 +1,26 @@
 head.ready(document, function () {
-	console.log($('body').html());
+
+	function tabs() {
+		var el = $('.js-tabs'),
+				el_tab = el.find('.price__menu a'),
+				el_list = el.find('.price__content'),
+				el_item = el.find('.price__item');
+
+
+			el_tab.on('click', function() {
+				el.find('.price__menu a').removeClass('is-active');
+				$(this).addClass('is-active');
+				return false;
+			});
+
+			$('.price__menu a').each(function() {
+				var el_id = $(this).attr("href");
+				$(this).on('click', function() {
+					$('.price__content').find('.price__item').removeClass('is-open');
+					$(el_id).addClass('is-open');
+				});
+			});
+
+	} tabs();
+
 });
